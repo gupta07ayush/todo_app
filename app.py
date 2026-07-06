84f1aa0ab0ba63f46bf39a067a0812b325572d7b
Ayush Gutpa <gupta07ayush@gmail.com>
2026-07-06 17:00:52 +0530

    restructured file

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)
+
+#database setup
+def init_db():
+    conn = sqlite3.connect('todos.db')
+    conn.execute('''CREATE TABLE IF NOT EXISTS todos ( id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL, completed BOOLEAN NOT NULL DEFAULT 0, created_at TEXT NOT NULL)''')
+    conn.close()
+
+init_db() #create table on startup
+
+#Routes
+@app.route("/", methods=['GET',"POST"])
+def index():
+    if request.method == "POST":
+        task = request.form.get('task')
+        if task:
+            conn = sqlite3.connect('todos.db')
+            conn.execute("INSERT INTO todos (task, created_at) VALUES(?, ?)",(task,datetime.now().strftime("%Y-%m-%d %H:%M")))
+            conn.commit()
+            conn.close()
+        return redirect(url_for("index"))
+    
+    #GET request - show all todos
+    conn = sqlite3.connect('todos.db')
+    todos = conn.execute("SELECT * FROM todos ORDER BY created_at DESC").fetchall()
+    conn.close()
+
+    return render_template("index.html", todos=todos)
+
+
+@app.route("/toggle/<int:todo_id>")
+def toggle(todo_id):
+    conn = sqlite3.connect("todos.db")
+    conn.execute("UPDATE todos SET completed = NOT completed WHERE id = ?",(todo_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    conn = sqlite3.connect('todos.db')
+    conn.execute("DELETE FROM todos WHERE id = ?", (todo_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
